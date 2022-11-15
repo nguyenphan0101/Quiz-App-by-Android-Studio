61a3b23b8fe1ed94432d1e3d24d8c1d7534fee16
nguyenphan0101 <nguyenphan01012002@gmail.com>
2022-11-15 15:23:10 +0700

    Adding Android Studio File

---
+++ app/src/main/java/com/example/project_x/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.project_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
